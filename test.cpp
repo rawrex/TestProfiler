f3fff0cebe1191e6e4b930b1d3a98a2fbbd9bd19
rawrex <rawrex@proton.me>
2022-07-26 15:24:13 +0700

    Cleaned up some text and refactored some test funcions out.

--- test.cpp
+++ test.cpp
@@ -4,54 +4,76 @@
 #include <random>
 #include <vector>
 
-struct CBase  
+constexpr unsigned ITERATION_MODULO = 3;
+
+struct Base  
 {
     virtual void Execute() = 0;
 		
-	virtual ~CBase() = default;
+	virtual ~Base() = default;
 
     std::atomic<bool> is_enabled;
     std::atomic<std::uint32_t> number_of_iterations;
 };
 
-class A : public CBase
+class A : public Base
 {
 	// Some heavy data
+	std::vector<long long> data;	
 };
 
-class B : public CBase
+class B : public Base
 {
 	// Some heavy data
+	std::vector<std::wstring> data;	
 };
 
-class C : public CBase
+class C : public Base
 {
 	// Some heavy data
+	std::vector<int64_t> data;	
 };
 
-class D : public CBase
+class D : public Base
 {
 	// Some heavy data
+	std::vector<long long> data;	
 };
 
 struct ItemInfo
 {
-    CBase* item;
+    Base* item;
 
 	std::atomic<bool> is_enabled; 
 	std::atomic<std::uint32_t> numebr_of_iterations; 
 };
 
+bool testIterations(const std::unique_ptr<Base>& object_ptr)
+{
+	return !(object_ptr->number_of_iterations % ITERATION_MODULO);
+}
+bool testEnabled(const std::unique_ptr<Base>& object_ptr)
+{
+	return object_ptr->is_enabled;
+}
+
+void testExecute(const std::unique_ptr<Base>& test_object_ptr)
+{
+	if(testEnabled(test_object_ptr) && testIterations(test_object_ptr))
+		test_object_ptr->Execute();
+}
+
+void testDirectObjectAccess(const std::vector<std::unique_ptr<Base>>& objects)
+{
+	// Note a reference here
+	for (const auto& object : objects)
+		testExecute(object);
+}
+
 int main()
 {
-    std::vector<CBase*> objects;
+    std::vector<std::unique_ptr<Base>> objects;
 
-    for (auto pObj : objects)
-    {
-        if (pObj->m_bIsEnabled && pObj->m_nIterations % 3 == 0)
-        {
-            pObj->Execute();
-        }
-    }
+    testDirectObjectAccess(objects);
 
 }
