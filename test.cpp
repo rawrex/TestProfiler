7d62645dd02f1866fc0451e8266b6c21434462a4
rawrex <rawrex@proton.me>
2022-07-26 20:21:45 +0700

    Cleaned up and set up the final version.

--- test.cpp
+++ test.cpp
@@ -20,13 +20,10 @@ using big_number_t = long long;
 using big_data_t = std::array<big_number_t, NUMBER_OF_DATUMS>;		// 80 bytes on my machine
 
 // Utility funcitons
-void print(const big_number_t& n) 
+template <typename T>
+void print(const T& msg, char delimiter = ' ')
 {
-	std::cout << n << std::endl;
-}
-void print(const std::string& msg, const char delim = ' ')
-{
-	std::cout << msg << delim;
+	std::cout << msg << delimiter;
 }
 
 big_number_t makeRandomNumber(long long min, long long max)
@@ -80,7 +77,8 @@ public:
 
 	virtual void Execute() override 
 	{
-		print(__PRETTY_FUNCTION__, '\n');
+		// print(__PRETTY_FUNCTION__, '\n');
+		print(makeRandomNumber(0,10));
 	}
 
 private:
@@ -96,7 +94,8 @@ public:
 
 	virtual void Execute() override 
 	{
-		print(__PRETTY_FUNCTION__, '\n');
+		// print(__PRETTY_FUNCTION__, '\n');
+		print(makeRandomNumber(0,10));
 	}
 
 private:
@@ -111,7 +110,8 @@ public:
 
 	virtual void Execute() override 
 	{
-		print(__PRETTY_FUNCTION__, '\n');
+		// print(__PRETTY_FUNCTION__, '\n');
+		print(makeRandomNumber(0,10));
 	}
 
 private:
@@ -126,7 +126,8 @@ public:
 
 	virtual void Execute() override 
 	{
-		print(__PRETTY_FUNCTION__, '\n');
+		// print(__PRETTY_FUNCTION__, '\n');
+		print(makeRandomNumber(0,10));
 	}
 
 private:
@@ -146,25 +147,13 @@ std::shared_ptr<Base> makeRandomObject()
 	auto type = randomType();
 
 	if(type == TestType::A)
-	{
-		print("A");
 		return std::shared_ptr<Base>(new A());
-	}
 	if(type == TestType::B)
-	{
-		print("B");
 		return std::shared_ptr<Base>(new B());
-	}
 	if(type == TestType::C)
-	{
-		print("C");
 		return std::shared_ptr<Base>(new C());
-	}
 	if(type == TestType::D)
-	{
-		print("D");
 		return std::shared_ptr<Base>(new D());
-	}
 
 	throw std::exception();
 }
@@ -196,7 +185,7 @@ void testExecute(const std::shared_ptr<Base>& test_object_ptr)
 		test_object_ptr->Execute();
 }
 // Our first main test, in which we fetch objects directly without a pointer proxy
-void testDirectObjectAccess(const std::vector<std::shared_ptr<Base>>& objects)
+void testDirectAccess(const std::vector<std::shared_ptr<Base>>& objects)
 {
 	// Note a reference here
 	for (const auto& object : objects)
@@ -206,16 +195,51 @@ void testDirectObjectAccess(const std::vector<std::shared_ptr<Base>>& objects)
 
 struct ItemInfo
 {
-	ItemInfo(const std::shared_ptr<Base> object) : item(object)
+	ItemInfo(const std::shared_ptr<Base>& object) : item(object)
 	{
 		is_enabled.store(object->is_enabled);
 		number_of_iterations.store(object->number_of_iterations);
+	}
+
+	ItemInfo(ItemInfo&& item_info) : item(std::move(item_info.item))
+	{
+		is_enabled.store(item_info.is_enabled);
+		number_of_iterations.store(item_info.number_of_iterations);
 	}
 
     std::shared_ptr<Base> item;
 	std::atomic<bool> is_enabled; 
 	std::atomic<std::uint32_t> number_of_iterations; 
 };
+std::vector<ItemInfo> makeTestPointers(const std::vector<std::shared_ptr<Base>>& test_objects)
+{
+	std::vector<ItemInfo> test_pointers;
+
+	for(const std::shared_ptr<Base>& object : test_objects)
+		test_pointers.emplace_back(object);
+
+	return test_pointers;
+}
+bool testIterations(const ItemInfo& item_info)
+{
+	return !(item_info.number_of_iterations % ITERATION_MODULO);
+}
+bool testEnabled(const ItemInfo& item_info)
+{
+	return item_info.is_enabled;
+}
+void testExecute(const ItemInfo& item_info)
+{
+	if(testEnabled(item_info) && testIterations(item_info))
+		item_info.item->Execute();
+}
+void testIndirectAccess(const std::vector<ItemInfo>& ptr_objects)
+{
+	// Note a reference here
+	for (const auto& object : ptr_objects)
+		testExecute(object);
+}
+		
 
 
 // Profiler function
@@ -231,13 +255,33 @@ double timedExecution(F&& f, Ts&&...args)
 
 int main()
 {
-	std::cout << "\n\nSize of big_data_t: " << sizeof(big_data_t) << std::endl;
-	std::cout << "Size of a descendant class (e.g. class A): " << sizeof(A) << std::endl;
+	// Prepare the stdout to display the results properly
+	std::cout.precision(8);
+	std::cout << std::fixed;
 
-	print("Start generation of test object", '\n');
+	print("Size of big_data_t: ");
+	print(sizeof(big_data_t), '\n');
+	print("Size of a descendant class (e.g. class A): ");
+	print(sizeof(A), '\n');
+
+	print("Start generation of test objects...");
     std::vector<std::shared_ptr<Base>> objects = makeTestObjects();
+	print("Done!", '\n');
 
-	print("\nFinished generation of objects", '\n');
-    std::cout << timedExecution(testDirectObjectAccess, objects);
+	print("Start generation of pointers to objects...");
+	auto pointers = makeTestPointers(objects);
+	print("Done!", '\n');
+	
+	std::cout << "Testing direct access... " << std::flush;
+    auto direct_result = timedExecution(testDirectAccess, objects);
+	std::cout << "Done!" << std::endl;
+	std::cout << "Testing indirect access... " << std::flush;
+    auto indirect_result = timedExecution(testIndirectAccess, pointers);
+	std::cout << "Done!" << std::endl;
+
+	print("Direct access result:");
+	print(direct_result, '\n');
+	print("Indirect access result:");
+	print(indirect_result);
 
 }
