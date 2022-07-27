c46cd379d35e6a88f3945510032ab9b7eedd318c
rawrex <rawrex@proton.me>
2022-07-27 20:21:09 +0700

    Moved testing function to a separate file.

--- Functions.h
+++ Functions.h
@@ -21,8 +21,30 @@ double timedExecution(F&& f, Ts&&...args)
     return static_cast<double>(std::clock() - start) / static_cast<double>(CLOCKS_PER_SEC);
 }
 
-big_number_t makeRandomNumber(long long, long long);
+// Test framework
+template <typename T>
+bool testIterations(const T& object)
+{
+	return !(object.number_of_iterations % ITERATION_MODULO);
+}
+template <typename T>
+bool testEnabled(const T& object)
+{
+	return object.is_enabled;
+}
+template <typename T>
+bool test(const T& object)
+{
+	return testEnabled(object) && testIterations(object);
+}
+template <typename T>
+void testExecute(const T& object)
+{
+	if(test(object))
+		object.Execute();
+}
 
+big_number_t makeRandomNumber(long long, long long);
 
 big_data_t makeData();
 
