f11c343d4abc7fcf26257e39d4b03b4f7646bec4
rawrex <rawrex@proton.me>
2022-07-28 21:03:39 +0700

    Added a set of overloaded functions Execute.

--- NoVirtual.h
+++ NoVirtual.h
@@ -2,6 +2,7 @@
 #define NOVIRTUAL_H
 
 #include <atomic>
+#include "Factory.h"
 #include "Constants.h"
 #include "Functions.h"
 
@@ -69,22 +70,22 @@ private:
 	big_data_t data;
 };
 
-struct Proxy
+struct NoVirtualProxy
 {
-	Proxy() = default;
-	Proxy(Proxy&& item_info) : item(std::move(item_info.item))
+	NoVirtualProxy() = default;
+	NoVirtualProxy(NoVirtualProxy&& item_info) : item(std::move(item_info.item))
 	{
 		is_enabled.store(item_info.is_enabled);
 		number_of_iterations.store(item_info.number_of_iterations);
 	}
-	Proxy& operator=(const Proxy& item_info) 
+	NoVirtualProxy& operator=(const NoVirtualProxy& item_info) 
 	{
 		item = item_info.item;
 		is_enabled.store(item_info.is_enabled);
 		number_of_iterations.store(item_info.number_of_iterations);
 		return *this;
 	}
-	Proxy(const NoVirtualBase& object) : item(&object) 
+	NoVirtualProxy(const NoVirtualBase& object) : item(&object) 
 	{
 		is_enabled.store(object.is_enabled);
 		number_of_iterations.store(object.number_of_iterations);
@@ -95,4 +96,20 @@ struct Proxy
 	std::atomic<std::uint32_t> number_of_iterations = 0;
 };
 
+void Execute(const NoVirtualA& object)
+{
+	object.Execute();
+}
+void Execute(const NoVirtualB& object)
+{
+	object.Execute();
+}
+void Execute(const NoVirtualC& object)
+{
+	object.Execute();
+}
+void Execute(const NoVirtualD& object)
+{
+	object.Execute();
+}
 #endif
