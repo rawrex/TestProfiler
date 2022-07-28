3ccd5805304f66192c0d1707b5880aaf000244b2
rawrex <rawrex@proton.me>
2022-07-28 20:51:02 +0700

    Introduced a version of the test classes without any virtual functions.

---
+++ NoVirtual.h
@@ -0,0 +1,101 @@
+#ifndef NOVIRTUAL_H
+#define NOVIRTUAL_H
+
+#include <atomic>
+#include "Constants.h"
+#include "Functions.h"
+
+struct NoVirtualBase  
+{
+	NoVirtualBase() 
+		: is_enabled(makeRandomBool()), number_of_iterations(makeRandomIterationsNumber()) {}
+	virtual ~NoVirtualBase() = default;
+
+    std::atomic<bool> is_enabled;
+    std::atomic<uint32_t> number_of_iterations;
+};
+
+class NoVirtualA : public NoVirtualBase
+{
+public:
+	NoVirtualA() : data(makeData()) {}
+	virtual ~NoVirtualA() = default;
+	void Execute() const 
+	{
+		print(makeRandomNumber(0,10));
+	}
+
+private:
+	big_data_t data;
+};
+
+class NoVirtualB : public NoVirtualBase
+{
+public:
+	NoVirtualB() : data(makeData()) {}
+	virtual ~NoVirtualB() = default;
+	void Execute() const 
+	{
+		print(makeRandomNumber(0,10));
+	}
+private:
+	big_data_t data;
+};
+
+class NoVirtualC : public NoVirtualBase
+{
+public:
+	NoVirtualC() : data(makeData()) {}
+	virtual ~NoVirtualC() = default;
+	void Execute() const 
+	{
+		print(makeRandomNumber(0,10));
+	}
+private:
+	big_data_t data;
+};
+
+class NoVirtualD : public NoVirtualBase
+{
+public:
+	NoVirtualD() : data(makeData()) {}
+	virtual ~NoVirtualD() = default;
+	void Execute() const 
+	{
+		print(makeRandomNumber(0,10));
+	}
+
+private:
+	big_data_t data;
+};
+
+struct Proxy
+{
+	Proxy() = default;
+	Proxy(Proxy&& item_info) : item(std::move(item_info.item))
+	{
+		is_enabled.store(item_info.is_enabled);
+		number_of_iterations.store(item_info.number_of_iterations);
+	}
+	Proxy& operator=(const Proxy& item_info) 
+	{
+		item = item_info.item;
+		is_enabled.store(item_info.is_enabled);
+		number_of_iterations.store(item_info.number_of_iterations);
+		return *this;
+	}
+	Proxy(const NoVirtualBase& object) : item(&object) 
+	{
+		is_enabled.store(object.is_enabled);
+		number_of_iterations.store(object.number_of_iterations);
+	}
+	void Execute() const 
+	{
+		item->Execute();
+	}
+	const NoVirtualBase* item = nullptr;
+	std::atomic<bool> is_enabled = false; 
+	std::atomic<std::uint32_t> number_of_iterations = 0;
+};
+
+#endif
